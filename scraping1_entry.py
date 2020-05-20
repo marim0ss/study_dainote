c14a541e2bdb3e975e53dcd2fa0f47b9680fc42f
marim0ss <piyomaru.bunbun@gmail.com>
2020-05-20 17:56:04 +0900

    output to csv

--- scraping1_entry.py
+++ scraping1_entry.py
@@ -4,34 +4,6 @@ import requests
 import pandas as pd
 import pprint
 
-# こちらで用意したHTML
-html = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
-<p class="story">...</p>
-"""
-# BeautifulSoupの初期化
-soup = BeautifulSoup(html, 'html.parser') # BeautifulSoupの初期化
-# print(soup.prettify())
-
-# print(soup.title)  # titleタグの内容ごと取得
-# print(soup.title.string) # titleタグの中身を取得(k中身が分断されてなければ)
-# print(soup.body.text)      # bodyのテキスト部分のみ
-tags = soup.find_all("a")
-# pprint.pprint(tags)
-
-for a_html in tags:     # aタグのテキストを抜き出し
-    print(a_html.text)
-
-#soup.a.get("href")     # aタグ内のURL部分（href）を取得する
-for link in tags:
-    print(link.get("href"))
 # -------------------------------------------------------
 # 実際のサイトのスクレイピング
 # -------------------------------------------------------
@@ -75,9 +47,15 @@ for tag in p_tags:
     url = tag.a.get("href")
     # 行の作成
     se = pd.Series([name, url], columns)
-    print(se)
+    # print(se)
     df2 = df2.append(se, columns) # データフレームに行を追加
     print(df2)
-
+"""
+# -------------------------------------------------------
 # 作成したデータフレームをCSVに変換
-#df.to_csv(“ファイル名.csv”)でcsvファイルを作成できます。
+#pandasの.to_csv(“ファイル名.csv”)でcsvファイルを作成できます。
+# -------------------------------------------------------
+"""
+filename = '../study_dainote/cf_information.csv'
+df2.to_csv(filename, encoding = 'utf-8')
+print('csvファイルを出力しました')
